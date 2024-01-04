6694702e334a03f331d9cb1be7a5da5f017f0e93
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-01-04 20:46:39 +0530

    Added new constraint for foreign key

---
+++ src/migrations/20240104150255-update-city-airport-association.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addConstraint('Airports', {
+      type: 'FOREIGN KEY',
+      name: 'city_fk_constraint',
+      fields: ['cityId'],
+      references: {
+        table: 'Cities',
+        field: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeConstraint('Airports', 'city_fk_constraint');
+  }
+};
