62cdcc25ca81bdabd736c960b46998acfa0e6866
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-01-07 00:32:50 +0530

    Added Seats model associated to airplaneId

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -14,7 +14,11 @@ module.exports = (sequelize, DataTypes) => {
       this.hasMany(models.Flight, {
         foreignKey: 'airplaneId',
         onDelete: 'CASCADE'
-      })
+      });
+      this.hasMany(models.Seat, {
+        foreignKey: 'airplaneId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   Airplane.init({
