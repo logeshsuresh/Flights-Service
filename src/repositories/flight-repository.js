653f997c3b7ee16a059ed8c0fa30462690a59a0d
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-01-09 20:39:31 +0530

    Segregated raw queries

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 const CrudRepository = require("./crud-repository");
 const { Flight, Airplane, Airport, City } = require('../models');
 const db = require('../models');
+const { addRowLockOnFlights } = require('./queries');
 
 class FlightRepository extends CrudRepository {
     constructor() {
@@ -49,7 +50,7 @@ class FlightRepository extends CrudRepository {
     }
 
     async updateRemainingSeats(flightId, seats, dec = 1) {
-        await db.sequelize.query(`SELECT * FROM Flights WHERE Flights.id = ${flightId} FOR UPDATE`);
+        await db.sequelize.query(addRowLockOnFlights(flightId));
         const flight = await Flight.findByPk(flightId);
         if (parseInt(dec)) {
             await flight.decrement('totalSeats', {by: seats});
