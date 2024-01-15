17567d868285d71be613fc5f0d30c05f17b63afe
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-01-15 21:45:34 +0530

    Added info controller to check proxy

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,7 @@ module.exports = {
     AirplaneController: require('./airplane-controller'),
     CityController: require('./city-controller'),
     AirportController: require('./airport-controller'),
-    FlightController: require('./flight-controller')
+    FlightController: require('./flight-controller'),
+    InfoController: require('./info-controller')
 }
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+const { InfoController } = require('../../controllers');
 const airplaneRoutes = require('./airplane-routes');
 const cityRoutes = require('./city-routes');
 const airportRoutes = require('./airport-routes');
@@ -11,5 +12,6 @@ router.use('/airplanes', airplaneRoutes);
 router.use('/cities', cityRoutes);
 router.use('/airports', airportRoutes);
 router.use('/flights', flightRoutes);
+router.get('/info', InfoController.info);
 
 module.exports = router;
